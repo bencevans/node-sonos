0360d9b8a76265d9e98a63d461b349403cf11caf
Ben Evans <ben@bensbit.co.uk>
2013-02-24 22:58:57 +0000

    queuenext example accept SONOS_HOST env

--- examples/queuenext.js
+++ examples/queuenext.js
@@ -1,6 +1,6 @@
 
 var Sonos = require('../').Sonos;
-var sonos = new Sonos('192.168.2.11');
+var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11');
 
 sonos.queueNext('http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3', function(err, playing) {
   console.log([err, playing]);
