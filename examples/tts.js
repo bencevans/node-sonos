fe9d9fc5c0ff50162788918145e2083cdc24bb42
Stephan van Rooij <stephan@svrooij.nl>
2013-11-04 11:53:57 +0100

    Updated TTS example

--- examples/tts.js
+++ examples/tts.js
@@ -1,10 +1,17 @@
 
-var webhost = '192.168.2.25';
-var text = 'Yo, play some sweet music, Not This!';
+//var webhost = '192.168.2.25';
+var text = 'Ben Evens made a nice node JS library for your sonos system';
 
 var Sonos = require('../');
 var sonos = new Sonos.Sonos('192.168.2.14');
 
-sonos.queueNext('http://translate.google.com/translate_tts?q=#{text}&tl=en.mp3', function(err, playing) {
+//Replace all spaces with a _ because Sonos doesn't support spaces
+text = text.replace(/ /g,'_');
+
+//For supported languages see www.voicerss.org/api/documentation.aspx
+//This url just redirects to voicerss because of the specific url format for the sonos
+var url = 'http://i872953.iris.fhict.nl/speech/en-us_' + encodeURIComponent(text)+'.mp3';
+
+sonos.queueNext(url, function(err, playing) {
   console.log([err, playing]);
 });
