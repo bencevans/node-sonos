95d2a583f6f8078b7f7007deb3e4f17b00bd0789
Ben Evans <ben@bensbit.co.uk>
2014-01-07 16:33:35 +0000

    Add grunt, jshint & travis

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: [
+          '*.min.js',
+          'node_modules/**/*',
+          'public/js/components/**',
+          'coverage/**'
+        ]
+      },
+      all: [
+        '*.js',
+        '**/*.js'
+      ]
+
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'dot',
+        },
+        src: ['test/**.js']
+      }
+    },
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('test', ['mocha']);
+
+  if(process.env.TEST_CMD) {
+    grunt.registerTask('travis', process.env.TEST_CMD);
+  }
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
