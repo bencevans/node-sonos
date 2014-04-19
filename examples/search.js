5e467b4d43d15bd23fd62da187b5ed4789b084c9
Stephen Wan <stephen@stephenwan.net>
2014-04-19 13:46:49 -0500

    added support for checking device model type in search()

--- examples/search.js
+++ examples/search.js
@@ -2,6 +2,6 @@
 var Sonos = require('../');
 var search = Sonos.search();
 
-search.on('DeviceAvailable', function(device) {
-  console.log(device);
+search.on('DeviceAvailable', function(device, model) {
+  console.log(device, model);
 });
