715164cb23ad1d20d984d476dd987a7b0e3708f0
Stephen Wan <stephen@stephenwan.net>
2014-05-01 03:41:04 -0500

    updated svc number, random number generation

--- examples/playspotify.js
+++ examples/playspotify.js
@@ -2,6 +2,6 @@ var Sonos = require('../').Sonos;
 
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.19', process.env.SONOS_PORT || 1400);
 
-sonos.queueSpotify('6EKQbI0jKua5AehBsicMdD', function(err, data) {
+sonos.queueSpotify('6ouTGbETM7ZdID1eMXZJde', function(err, data) {
 	console.log(err, data);
 });
