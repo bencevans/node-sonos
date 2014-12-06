6c0f6e24740974c4112b46bbeb783a047e36739c
Stephen Wan <stephen@stephenwan.net>
2014-12-05 20:56:48 -0600

    working volume listener impl

---
+++ examples/volumeWatcher.js
@@ -0,0 +1,9 @@
+var Sonos = require('../index').Sonos;
+var Listener = require('../lib/events/listener');
+
+var device = new Sonos(process.env.SONOS_HOST || '192.168.2.11');
+device.startListeners(function() {
+  device.on('volumeChange', function(volume) {
+    console.log(volume);
+  });
+});
