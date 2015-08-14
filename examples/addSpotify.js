df9e4b1261e3473a46a7f567c726e8e05cfc1f78
Ben <benji889@yahoo.de>
2015-08-14 15:02:42 +0100

    more style changes

--- examples/addSpotify.js
+++ examples/addSpotify.js
@@ -3,7 +3,7 @@ var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
 
 var spotify_track_id = '5AdoS3gS47x40nBNlNmPQ8' // Slayer ftw
 
-sonos.addSpotify(spotify_track_id, function(err, res) {
+sonos.addSpotify(spotify_track_id, function (err, res) {
   console.log(err)
   console.log(res)
 })
