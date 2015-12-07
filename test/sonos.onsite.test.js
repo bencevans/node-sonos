fde53eba0d53d3325c91c1fe93e219ed668573a9
Ben Evans <ben@bensbit.co.uk>
2015-12-07 04:12:07 +0000

    Merge branch 'antxxxx-searchFix' + add very simple test

---
+++ test/sonos.onsite.test.js
@@ -0,0 +1,17 @@
+const sonos = require('../')
+
+describe('Search Music Library', function () {
+  var device = null
+
+  before(function (done) {
+    sonos.search(function (dev) {
+      device = dev
+      done()
+    })
+  })
+
+  it('returns search results from the Sonos library', function (done) {
+    // TODO: Verify data response
+    device.searchMusicLibrary('tracks', 'Newton', {}, done)
+  })
+})
