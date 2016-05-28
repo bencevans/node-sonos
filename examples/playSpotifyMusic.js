0f6fbc8186090399cfd13a12d23159b5943f0f8a
scotts@microsoft.com <scotts@microsoft.com>
2016-05-28 12:28:04 -0700

    Tests and examples for playing Spotify uris

---
+++ examples/playSpotifyMusic.js
@@ -0,0 +1,86 @@
+var async = require('async')
+var Sonos = require('../').Sonos
+var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
+
+// This example demonstrates playing various spotify uri types.
+// Just replace the spotify uri used in the Play command below
+// with your own. These uris can be obtained by using the Spotify
+// REST apis:
+//     https://developer.spotify.com/web-api/console/
+//
+// Or by using a scoped internet search and scraping the results:
+//    e.g. "A night at the opera site:spotify.com"
+//
+// Spotify uri examples:
+//     Bohemian Rhapsody track - spotify:track:1AhDOtG9vPSOmsWgNW0BEY
+//     A night at the opera album - spotify:album:1TSZDcvlPtAnekTaItI3qO
+//     Top tracks by Queen - spotify:artistTopTracks:1dfeR4HaWDbWqFHLkxsg1d
+//     Queen playlist (public user) - spotify:user:26iFraqozskd5POrzg68pr
+//     Spotify curated artist radio - spotify:artistRadio:1dfeR4HaWDbWqFHLkxsg1d
+//
+// The sample code below selects the queue, clears it, sets the volume 
+// and then plays the music through the Spotify service by specifying 
+// the appropriate Spotify uri.
+//
+// This assumes you have the Spotify music service connected to 
+// your Sonos system.
+
+playSpotifyUri(spotifyUri, (err, result) => {
+    console.log('sonosService playSpotifyTracks');
+
+    if (!artistId) {
+        return callback('Missing artistId param in playSpotifyUri');
+    }
+
+    if (!this.sonos) {
+        return callback('No Sonos controller found');
+    }
+    
+    async.series([
+        (next) => {
+            this.sonos.selectQueue((err, selectQueueResult) => {
+                if (err) {
+                    return next(err);
+                }
+                
+                return next(null);
+            });
+        },
+        
+        (next) => {
+            this.sonos.flush((err, flushQueueResult) => {
+                if (err) {
+                    return next(err);
+                }
+                
+                return next(null);
+            });
+        },
+
+        (next) => {
+            this.sonos.setVolume("25", (err, volumeResult) => {
+                if (err) {
+                    return next(err);
+                }
+                
+                return next(null);
+            });
+        },
+
+        (next) => {
+            this.sonos.play(spotifyUri, (err, playResult) => {
+                if (err) {
+                    return next(err);
+                }
+
+                return next(null);
+            });
+        }
+        
+    ], (err) => {
+        if (err) {
+            return callback('Error playing Spotify uri');
+        }
+        return callback(null, 'Succeeded');
+    });
+});
--- test/sonos.test.js
+++ test/sonos.test.js
@@ -92,6 +92,102 @@ describe('Sonos', function () {
     })
   })
 
+  describe('queue()', function () {
+    it('should generate queue command', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3</EnqueuedURI><EnqueuedURIMetaData></EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3', function () {})
+    })
+
+    it('should accept object in place of uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3</EnqueuedURI><EnqueuedURIMetaData>&lt;test&gt;&quot;hello&quot;&lt;/test&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue({
+        uri: 'http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3',
+        metadata: '<test>"hello"</test>'
+      }, function () {})
+    })
+
+    it('should accept a Spotify track uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>spotify%3atrack%3a3vCHmbyOgc6pgwLUWfU4pF</EnqueuedURI><EnqueuedURIMetaData>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;00032020spotify%3atrack%3a3vCHmbyOgc6pgwLUWfU4pF&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.item.audioItem.musicTrack&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3079_X_#Svc3079-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('spotify:track:1AhDOtG9vPSOmsWgNW0BEY', function () {})
+    })
+
+    it('should accept a Spotify album uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>x-rincon-cpcontainer:0004206cspotify%3aalbum%3a1TSZDcvlPtAnekTaItI3qO</EnqueuedURI><EnqueuedURIMetaData>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;0004206cspotify%3aalbum%3a1TSZDcvlPtAnekTaItI3qO&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.container.album.musicAlbum&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3079_X_#Svc3079-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('spotify:album:1TSZDcvlPtAnekTaItI3qO', function () {})
+    })
+
+    it('should accept a Spotify artist top tracks uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>x-rincon-cpcontainer:000e206cspotify%3aartistTopTracks%3a1dfeR4HaWDbWqFHLkxsg1d</EnqueuedURI><EnqueuedURIMetaData>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;000e206cspotify%3aartistTopTracks%3a1dfeR4HaWDbWqFHLkxsg1d&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.container.playlistContainer&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3079_X_#Svc3079-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('spotify:artistTopTracks:1dfeR4HaWDbWqFHLkxsg1d', function () {})
+    })
+
+    it('should accept a Spotify user public playlist uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>x-rincon-cpcontainer:0006206cspotify%3auser%3a26iFraqozskd5POrzg68pr</EnqueuedURI><EnqueuedURIMetaData>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;0006206cspotify%3auser%3a26iFraqozskd5POrzg68pr&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;&lt;/dc:title&gt;&lt;upnp:class&gt;object.container.playlistContainer&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3079_X_#Svc3079-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('spotify:user:26iFraqozskd5POrzg68pr', function () {})
+    })
+
+    it('should accept a Spotify artist radio uri', function (done) {
+      var sonos = new Sonos('localhost', 1400)
+
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
+        assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>spotify%3aartistRadio%3a1dfeR4HaWDbWqFHLkxsg1d</EnqueuedURI><EnqueuedURIMetaData>&lt;DIDL-Lite xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot; xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot; xmlns:r=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot; xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;&lt;item id=&quot;000c206cspotify%3aartistRadio%3a1dfeR4HaWDbWqFHLkxsg1d&quot; restricted=&quot;true&quot;&gt;&lt;dc:title&gt;Artist Radio&lt;/dc:title&gt;&lt;upnp:class&gt;object.item.audioItem.audioBroadcast.#artistRadio&lt;/upnp:class&gt;&lt;desc id=&quot;cdudn&quot; nameSpace=&quot;urn:schemas-rinconnetworks-com:metadata-1-0/&quot;&gt;SA_RINCON3079_X_#Svc3079-0-Token&lt;/desc&gt;&lt;/item&gt;&lt;/DIDL-Lite&gt;</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
+        done()
+      }
+
+      sonos.queue('spotify:artistRadio:1dfeR4HaWDbWqFHLkxsg1d', function () {})
+    })
+  })
+
   describe('queueNext()', function () {
     it('should generate queue command', function (done) {
       var sonos = new Sonos('localhost', 1400)
@@ -106,77 +202,20 @@ describe('Sonos', function () {
       sonos.queueNext('http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3', function () {})
     })
 
-    it('should accept a uri string', function (done) {
+    it('should accept object in place of uri', function (done) {
       var sonos = new Sonos('localhost', 1400)
 
-      var actionCounter = 0
-
-      sonos.request = function (endpoint, action, body, _, callback) {
-        actionCounter++
-
-        if (actionCounter === 1) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI"')
-          assert(body === '<u:SetAVTransportURI xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><CurrentURI>http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3</CurrentURI><CurrentURIMetaData></CurrentURIMetaData></u:SetAVTransportURI>')
-          callback(null, [{
-            FirstTrackNumberEnqueued: ['1'],
-            NewQueueLength: ['1'],
-            NumTracksAdded: ['1']
-          }])
-        } else if (actionCounter === 2) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#Seek"')
-          assert(body === '<u:Seek xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Unit>TRACK_NR</Unit><Target>1</Target></u:Seek>')
-          callback(null, [{
-            $: {'xmlns:u': 'urn:schemas-upnp-org:service:AVTransport:1'}
-          }])
-        } else if (actionCounter === 3) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#Play"')
-          assert(body === '<u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play>')
-          done()
-        }
-      }
-
-      sonos.queueNext('http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3')
-    })
-
-    it('should be able to accept an object instead of uri', function (done) {
-      var sonos = new Sonos('localhost', 1400)
-
-      var actionCounter = 0
-
-      sonos.request = function (endpoint, action, body, _, callback) {
-        actionCounter++
-
-        if (actionCounter === 1) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#AddURIToQueue"')
-          assert(body === '<u:AddURIToQueue xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><EnqueuedURI>http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3</EnqueuedURI><EnqueuedURIMetaData>test</EnqueuedURIMetaData><DesiredFirstTrackNumberEnqueued>0</DesiredFirstTrackNumberEnqueued><EnqueueAsNext>1</EnqueueAsNext></u:AddURIToQueue>')
-          callback(null, [{
-            FirstTrackNumberEnqueued: ['1'],
-            NewQueueLength: ['1'],
-            NumTracksAdded: ['1']
-          }])
-        } else if (actionCounter === 2) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#Seek"')
-          assert(body === '<u:Seek xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Unit>TRACK_NR</Unit><Target>1</Target></u:Seek>')
-          callback(null, [{
-            $: {'xmlns:u': 'urn:schemas-upnp-org:service:AVTransport:1'}
-          }])
-        } else if (actionCounter === 3) {
-          assert(endpoint === '/MediaRenderer/AVTransport/Control')
-          assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#Play"')
-          assert(body === '<u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play>')
-          done()
-        }
+      sonos.request = function (endpoint, action, body) {
+        assert(endpoint === '/MediaRenderer/AVTransport/Control')
+        assert(action === '"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI"')
+        assert(body === '<u:SetAVTransportURI xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><CurrentURI>http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3</CurrentURI><CurrentURIMetaData>&lt;test&gt;&quot;hello&quot;&lt;/test&gt;</CurrentURIMetaData></u:SetAVTransportURI>')
+        done()
       }
 
       sonos.queueNext({
         uri: 'http://livingears.com/music/SceneNotHeard/091909/Do You Mind Kyla.mp3',
-        metadata: 'test'
-      })
+        metadata: '<test>"hello"</test>'
+      }, function () {})
     })
   })
 })
