e8a6107b0e32e274769e687999165a5033ef838f
Stephan van Rooij <stephan.vanrooij@fontys.nl>
2016-11-13 17:09:13 +0100

    Adding transport event sample

---
+++ examples/transportEvent.js
@@ -0,0 +1,24 @@
+var Sonos = require('../index').Sonos
+var Listener = require('../lib/events/listener')
+
+var sonos = new Sonos(process.env.SONOS_HOST || '192.168.96.55')
+var x = new Listener(sonos)
+x.listen(function (err) {
+  if (err) throw err
+
+  x.addService('/MediaRenderer/AVTransport/Event', function (error, sid) {
+    if (error) throw err
+    console.log('Successfully subscribed, with subscription id', sid)
+  })
+
+  x.on('serviceEvent', function (endpoint, sid, data) {
+
+    //It's a shame the data isn't in a nice track object, but this might need some more work.
+    // At this moment we know something is changed, either the play state or an other song.
+    console.log('Received event from', endpoint, '(' + sid + ') with data:', data, '\n\n')
+    sonos.currentTrack(function (err, track) {
+      console.log(err, track)
+    })
+
+  })
+})
