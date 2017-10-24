57fedc88d54c721a55fe1618adc9f85b64060452
Shobhit Gupta <smartyshobhit@gmail.com>
2017-10-24 00:54:59 -0700

    Fix missing options param in example

--- examples/searchLibrary.js
+++ examples/searchLibrary.js
@@ -2,6 +2,6 @@ var Sonos = require('../').Sonos
 
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.1.19', process.env.SONOS_PORT || 1400)
 
-sonos.searchMusicLibrary('tracks', 'orange crush', function (err, data) {
+sonos.searchMusicLibrary('tracks', 'orange crush', {}, function (err, data) {
   console.log(err, data)
 })
