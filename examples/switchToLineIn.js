26f763ad60d5cde4abeb9952bde5c6db594d3dba
Stephan van Rooij <github@svrooij.nl>
2018-01-03 13:23:20 +0100

    chore(Examples): Switch to line-in lint error

--- examples/switchToLineIn.js
+++ examples/switchToLineIn.js
@@ -1,28 +1,27 @@
 var Sonos = require('../').Sonos
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.96.223')
 
-sonos.getZoneInfo((err,data)=>{
-    
-    if(err){
-        console.log('Got error %j', err)
-        return
-    }
+sonos.getZoneInfo((err, data) => {
+  if (err) {
+    console.log('Got error %j', err)
+    return
+  }
     // console.log('Got zone info %j', data)
     // The mac is needed for switch to a different channel
-    var macCleaned = data.MACAddress.replace(/:/g, '')
-    console.log('Cleaned mac %j',macCleaned)
+  var macCleaned = data.MACAddress.replace(/:/g, '')
+  console.log('Cleaned mac %j', macCleaned)
 
     // To swtich on a playbar do the following
-    var uri = 'x-sonos-htastream:RINCON_' + macCleaned + '01400:spdif'
+  var uri = 'x-sonos-htastream:RINCON_' + macCleaned + '01400:spdif'
 
     // To switch on a Play:5 do the following
-    //var uri = 'x-ricon-stream:RINCON_' + macCleaned + '01400'
+    // var uri = 'x-ricon-stream:RINCON_' + macCleaned + '01400'
 
-    sonos.playWithoutQueue(uri, (err, playing)=> {
-        if(err){
-            console.log('Got error %j',err)
-        } else {
-            console.log('Playing: %j',playing)
-        }
-    })
+  sonos.playWithoutQueue(uri, (err, playing) => {
+    if (err) {
+      console.log('Got error %j', err)
+    } else {
+      console.log('Playing: %j', playing)
+    }
+  })
 })
