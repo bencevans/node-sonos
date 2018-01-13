911ffc038a4c5fb02c6724ab497a9fda183e1217
Stephan van Rooij <github@svrooij.nl>
2018-01-13 22:57:19 +0100

    chore: Linting and some small errors

--- examples/play.js
+++ examples/play.js
@@ -2,9 +2,9 @@ var Sonos = require('../').Sonos
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
 
 sonos.play().then(result => {
-  console.log('Started playing %j',result)
+  console.log('Started playing %j', result)
 })
 
 sonos.getVolume().then(volume => {
-  console.log('The volume is %d',volume)
+  console.log('The volume is %d', volume)
 })
