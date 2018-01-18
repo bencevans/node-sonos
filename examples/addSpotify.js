a85a42d2c0650fa9435d8f69b6b89e5fc5fa33d5
Stephan van Rooij <stephan.vanrooij@fontys.nl>
2018-01-18 15:07:34 +0100

    fix: Spotify sample to async

--- examples/addSpotify.js
+++ examples/addSpotify.js
@@ -2,20 +2,11 @@ var SONOS = require('../')
 var Sonos = SONOS.Sonos
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
 
-var spotifyTrackTd = '5AdoS3gS47x40nBNlNmPQ8' // Slayer ftw
+var spotifyTrackTd = 'spotify:track:5AdoS3gS47x40nBNlNmPQ8' // Slayer ftw
+sonos.setSpotifyRegion(SONOS.SpotifyRegion.EU)
 
-sonos.addSpotify(spotifyTrackTd, function (err, res) {
-  console.log(err)
-  console.log(res)
-})
-
-//
-// or if you use spotify EU service
-//
-var sonosWithSpotifyEU = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
-sonosWithSpotifyEU.setRegion(SONOS.SpotifyRegion.EU)
-
-sonosWithSpotifyEU.addSpotify(spotifyTrackTd, function (err, res) {
-  console.log(err)
-  console.log(res)
+sonos.queue(spotifyTrackTd).then(result => {
+  console.log('Added spotify track to queue %j', result)
+}).catch(err => {
+  console.log('Error adding %j', err)
 })
