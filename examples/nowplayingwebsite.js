bf544d46c49b37760445bf589adc8c82d9e1b617
Stephan van Rooij <github@svrooij.nl>
2018-01-30 17:16:01 +0100

    docs: Fixed one sample deleted the other
    Fixes #219

--- examples/nowplayingwebsite.js
+++ examples/nowplayingwebsite.js
@@ -3,27 +3,26 @@ var Sonos = require('../').Sonos
 
 var sonos = new Sonos(process.env.SONOS_HOST || '192.168.2.11')
 
-var server = http.createServer(function (req, res) {
-  sonos.currentTrack(function (err, track) {
-    if (err) {
-      throw err
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    })
-
-    var rows = []
-
-    for (var key in track) {
-      if (key === 'albumArtURI') {
-        rows.push('<tr><th>' + key + '</th><td>' + track[key] + '</td></tr>')
-      }
-    }
-
-    res.write('<table>' + rows.join('') + '</table>')
-    res.end()
+var server = http.createServer(async function (req, res) {
+  var track = await sonos.currentTrack()
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
   })
-})
 
-server.listen(process.env.PORT || 3000)
+  var rows = []
+
+  for (var key in track) {
+    if (key === 'albumArtURL') {
+      rows.push('<tr><th>' + key + '</th><td><img src="' + track[key] + '"/></td></tr>')
+    } else {
+      rows.push('<tr><th>' + key + '</th><td>' + track[key] + '</td></tr>')
+    }
+  }
+
+  res.write('<table>' + rows.join('') + '</table>')
+  res.end()
+})
+const port = process.env.PORT || 3000
+server.listen(port)
+
+console.log('Listening on http://localhost:%d/ CTRL+C to exit', port)
