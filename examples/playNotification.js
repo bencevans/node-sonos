bbc07f16bd06fabffc232e62c462920c39790aad
Stephan van Rooij <github@svrooij.nl>
2018-01-31 20:16:22 +0100

    fix: Other source for doorbell sound

--- examples/playNotification.js
+++ examples/playNotification.js
@@ -2,8 +2,8 @@ const Sonos = require('../').Sonos
 const sonos = new Sonos(process.env.SONOS_HOST || '192.168.96.52')
 
 sonos.playNotification({
-  uri: 'http://www.orangefreesounds.com/wp-content/uploads/2017/10/Doorbell-sound-ringing-fast.mp3',
-  onlyWhenPlaying: true, // It will query the state anyway, don't play the notification if the speaker is currently off.
+  uri: 'https://archive.org/download/Doorbell_1/doorbell.mp3',
+  onlyWhenPlaying: false, // It will query the state anyway, don't play the notification if the speaker is currently off.
   volume: 4 // Change the volume for the notification, and revert back afterwards.
 }).then(result => {
   // It will wait until the notification is played until getting here.
