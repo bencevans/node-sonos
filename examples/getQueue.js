77577f74abf9835c4d4a1575f05ef39ca55db47a
Stephan van Rooij <github@svrooij.nl>
2018-12-04 23:13:24 +0100

    feat: Added removing items from the queue
    Fixes #368

--- examples/getQueue.js
+++
@@ -1,8 +0,0 @@
-const Sonos = require('../').Sonos
-const sonos = new Sonos(process.env.SONOS_HOST || '192.168.96.56')
-
-sonos.getQueue().then(result => {
-  console.log('Current queue: %s', JSON.stringify(result, null, 2))
-}).catch(err => {
-  console.log('Error fetch queue %j', err)
-})
