bca2399dfc426b7374c1982785aa41fb3a1de82c
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2021-09-29 23:27:50 +0200

    chore: Converted Device discovery to class

--- examples/deviceDiscovery.js
+++ examples/deviceDiscovery.js
@@ -1,20 +1,20 @@
 const Sonos = require('../')
 
-console.log('Searching for Sonos devices for 5 seconds...')
+console.log('Searching for Sonos devices for 10 seconds...')
 
 const discovery = new Sonos.AsyncDeviceDiscovery()
 
-discovery.discover().then((device, model) => {
+discovery.discover({ port: 50333, timeout: 10000 }).then((device, model) => {
   console.log('Found one sonos device %s getting all groups', device.host)
   return device.getAllGroups().then((groups) => {
     console.log('Groups %s', JSON.stringify(groups, null, 2))
     return groups[0].CoordinatorDevice().togglePlayback()
   })
 }).catch(e => {
-  console.warn(' Error in discovery %j', e)
+  console.warn(' Error in discovery %o', e)
 })
 
-// discovery.discoverMultiple({ timeout: 5000 }).then((devices) => {
+// discovery.discoverMultiple({ port: 50333, timeout: 10000 }).then((devices) => {
 //   console.log('Found %d sonos devices', devices.length)
 //   devices.forEach(device => {
 //     console.log('Device IP: %s', device.host)
